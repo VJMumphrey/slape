f1218a02be42963359d3d2bc36850c30860c94b9
NathanielMitchell <dragonfire82003@gmail.com>
2025-02-14 23:15:14 -0600

    I made the guy functional like he work fr fr and now he model

--- SLaMO_Frontend/client/src/Prompt.tsx
+++ SLaMO_Frontend/client/src/Prompt.tsx
@@ -5,19 +5,21 @@ import MenuTabs from "./MenuTabs.tsx";
 // import {colors} from "../../../../../../AppData/Local/deno/npm/registry.npmjs.org/debug/4.3.7/src/browser.js";
 
 export default function Prompt() {
-  const [PromptInfo, setPromptInfo] = useState(""); //used to contain the current value, and to set the new value
+  const [ PromptInfo, setPromptInfo ] = useState(""); //used to contain the current value, and to set the new value
+  const [ ResponseAnswer, setResponseAnswer ] = useState("... Awaiting Response");
 
   async function handleSubmit(event: {preventDefault: () => void}){
+    setPromptInfo(""); //clears the prompt box after submission
+    setResponseAnswer(<><p>{`Prompt: ${PromptInfo}`}</p> <p>{`Response: Generating Response...`}</p></>);
     event.preventDefault(); //makes sure the page doesn't reload when submitting the form
     const response = await fetch("http://localhost:3069/simple", {
       method:"POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({prompt: PromptInfo})
+      body: JSON.stringify({prompt: PromptInfo, model: "gay", mode: "vito is"})
     });
-    alert(response);
-    setPromptInfo(""); //clears the prompt box after submission
+    setResponseAnswer(<><p>{`Prompt: ${PromptInfo}`}</p> <p>{`Response: ${JSON.parse(JSON.stringify(await response.json())).answer}`}</p></>);
   };
 
   // async function handleSubmit(event: {preventDefault: () => void}) {
@@ -29,7 +31,7 @@ export default function Prompt() {
   return (
     <>
       <MenuTabs/>
-      <div className="output"> Here's where the output will go</div>
+      <div className="output">{ResponseAnswer}</div>
       <div className="fixedBottom">
         <form onSubmit={handleSubmit}>
           <label>
