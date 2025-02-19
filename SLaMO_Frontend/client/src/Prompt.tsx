1e543650aa2dcbffb479248f8e375e5c2b87d7f6
Stone Gorman <smg062@latech.edu>
2025-02-19 15:16:59 -0600

    made the guy prettier, and moved some css guys

--- SLaMO_Frontend/client/src/Prompt.tsx
+++ SLaMO_Frontend/client/src/Prompt.tsx
@@ -5,27 +5,35 @@ import MenuTabs from "./MenuTabs.tsx";
 export default function Prompt() {
   const [PromptInfo, setPromptInfo] = useState(""); //used to contain the current value, and to set the new value
   const [ResponseAnswer, setResponseAnswer] = useState("... Awaiting Response");
-  const [ PromptMode, setPromptMode ] = useState("simple");
+  const [PromptMode, setPromptMode] = useState("simple");
 
-  const promptTypes = [{name: "Simple", type: "simple"}, {name: "Chain of Thought", type: "cot"}, {name: "Tree of Thought", type: "tot"}, {name: "Graph of Thought", type: "got"}, {name: "Thinking Hats", type: "thinkinghats"}];
+  const promptTypes = [
+    {name: "Simple", type: "simple"},
+    {name: "Chain of Thought", type: "cot"},
+    {name: "Tree of Thought", type: "tot"},
+    {name: "Graph of Thought", type: "got"},
+    {name: "Thinking Hats", type: "thinkinghats"},
+  ];
 
   function dropDownButton() {
     return (
-    <select value={PromptMode} onChange={(event) =>{setPromptMode(event.target.value);}}>
-      {promptTypes.map(
-        (type) => {
+      <select
+        className="inference"
+        value={PromptMode}
+        onChange={(event) => {
+          setPromptMode(event.target.value);
+        }}
+      >
+        {promptTypes.map((type) => {
           return (
-          <option value={type.type} key={type.type}>
-            {type.name}
-          </option>
-          )
-        }
-      )}
-    </select>
-    )
+            <option value={type.type} key={type.type}>
+              {type.name}
+            </option>
+          );
+        })}
+      </select>
+    );
   }
-
-
 
   async function handleSubmit(event: {preventDefault: () => void}) {
     event.preventDefault();
